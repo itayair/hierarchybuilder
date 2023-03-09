1bcef91bb21c2cb49ef41d7e94dcc009370759c5
iy245 <iy24592@gmail.com>
2023-03-09 13:59:02 +0200

    debug cycles

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="hierarchybuilder",
-    version="0.0.7",
+    version="0.0.8",
     author="Itay Yair",
     author_email="iy24592@gmail.com",
     description="implementation of hierarchy builder",
