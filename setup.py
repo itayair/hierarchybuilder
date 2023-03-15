10d49e9e9350c8369e6ff3d0423a580840ef4bee
iy245 <iy24592@gmail.com>
2023-03-15 13:48:25 +0200

    debug cycles

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="hierarchybuilder",
-    version="0.0.32",
+    version="0.1.4",
     author="Itay Yair",
     author_email="iy24592@gmail.com",
     description="implementation of hierarchy builder",
@@ -13,34 +13,19 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     license='MIT',
     url="https://github.com/itayair//hierarchybuilder",
-    # install_requires=[
-    #     'quantulum3==0.8.1',
-    #     'scipy==1.10.1',
-    #     'torch==1.13.1',
-    #     'spacy-legacy==3.0.12',
-    #     'spacy==3.0.9',
-    #     'spacy-loggers==1.0.4',
-    #     'transformers==4.26.1',
-    #     'uvicorn==0.20.0',
-    #     'fastapi==0.92.0',
-    #     'huggingface-hub==0.12.1',
-    #     'numpy==1.24.2',
-    #     'scikit-learn==1.2.0',
-    #     'sklearn==0.0.post1'
-    # ],
     install_requires=[
-        'fastapi>=0.90.0',
-        'nltk>=3.8.1',
-        'pandas>=1.5.0',
-        'quantulum3>=0.8.1',
-        'requests>=2.28.2',
-        'scikit_learn>=1.2.0',
-        'setuptools>=60.2.0',
-        'spacy>=3.0.6',
-        'torch>=1.10.1',
-        'tqdm>=4.64.0',
-        'transformers>=4.11.3',
-        'uvicorn>=0.20.0'
+        'fastapi==0.94.1',
+        'nltk==3.8.1',
+        'pandas==1.5.3',
+        'quantulum3==0.8.1',
+        'requests==2.28.2',
+        'scikit_learn==1.2.2',
+        # 'setuptools==60.2.0',
+        'spacy==3.0.69',
+        'torch==1.13.1',
+        'tqdm==4.64.1',
+        'transformers==4.26.1',
+        'uvicorn==0.21.0'
     ],
     dependency_links=['en-ud-model-sm @ https://storage.googleapis.com/en_ud_model/en_ud_model_sm-2.0.0.tar.gz'],
     project_urls={
@@ -52,5 +37,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
-    python_requires=">=3.6"
+    python_requires=">=3.7"
 )
